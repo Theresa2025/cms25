b5e8ad677de1314c63926542ee1c10b410ef6ea6
Theresa Kreindl <s2410456015@fhooe.at>
2025-10-28 14:05:55 +0100

    Initial check in

---
+++ packages/projekt25/README.md
@@ -0,0 +1,4 @@
+Site Package for the project "Projekt 25"
+==============================================================
+
+Add some explanation here.
---
+++ packages/projekt25/ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('TYPO3') or die('Access denied.');
+
+// Add default RTE configuration
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['projekt25'] = 'EXT:projekt25/Configuration/RTE/Default.yaml';
