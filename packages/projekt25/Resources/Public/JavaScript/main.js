ad624838e801e6a7578e30914a4a32fcca0169e8
Theresa Kreindl <s2410456015@fhooe.at>
2025-12-01 15:19:52 +0100

    01.12: Notfälle fomatiert, Team fomatiert, wollte Login einbinden hat noch nicht geklappt, Leistungen fomatiert, Footer flexiebel gemacht

---
+++ packages/projekt25/Resources/Public/JavaScript/main.js
@@ -0,0 +1,18 @@
+
+    document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.toggle-info-btn');
+    buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+    const extraInfo = this.nextElementSibling;
+    if (extraInfo.style.display === 'none') {
+    extraInfo.style.display = 'block';
+    this.textContent = '−'; // Minus anzeigen
+} else {
+    extraInfo.style.display = 'none';
+    this.textContent = '+'; // Plus zurück
+}
+});
+});
+});
+
+
